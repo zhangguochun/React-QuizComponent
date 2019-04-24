dc16cd3a0d7a138bae410f91044ac28529f28f6b
Gavin Zhang <gavin.zhang@activenetwork.com>
2019-04-24 17:16:28 +0800

    step 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,15 +10,19 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
     showNextQuestion(){
+        this.setState( {quiz_position: this.state.quiz_position+1});
+    }
+    handleResetClick(){
         this.setState((state) => {
-            return {quiz_position: state.quiz_position+1};
+            return {quiz_position:1};
         });
     }
     render() {
         const isQuizEnd = ((this.state.quiz_position -1) === quizData.quiz_questions.length)
         return(
             <div>
-                {isQuizEnd ? <QuizEnd /> :
+                {isQuizEnd ? <QuizEnd 
+                    resetClickHandler={this.handleResetClick.bind(this)}/> :
                 <QuizQuestion 
                     quiz_question={quizData.quiz_questions[this.state.quiz_position -1]}
                     showNextQuestionHandler={this.showNextQuestion.bind(this)}
--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 
 class QuizEnd extends Component {
 
+  handleResetclick() {
+    this.props.resetClickHandler();
+  }
   render () {
     return (
       <div>
         <p>Thanks for playing!</p>
-        <a href=''>Reset Quiz</a>
+        <a href='#' onClick={this.handleResetClick.bind(this)}>Reset Quiz</a>
       </div>
     )
   }
